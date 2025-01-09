0a9f5e62287507cd01c6216353f39b31e83281b0
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-01-09 16:52:13 +0100

    services update

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -6,8 +6,17 @@
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
     // TODO: Implémenter une fonction générique de cache
+    // Pour le moment, nous allons juste stocker les données dans Redis
+    const redisClient = await getRedisClient();
+    await redisClient.set(key, JSON.stringify(data), 'EX', ttl);
+
   }
-  
+  async function getData(key) {
+    const data=await redisClient.get(key);
+    return data ?JSON.parse(data):null;
+  }
   module.exports = {
     // TODO: Exporter les fonctions utilitaires
+    cacheData,
+    getData
   };
