db9aac0f3b8cdfd9d3ec8aed775faba089f39dea
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-01-09 14:40:29 +0100

    base de donnée terminée

---
+++ learning-platform-template/src/config/env.js
@@ -0,0 +1,37 @@
+// Question: Pourquoi est-il important de valider les variables d'environnement au démarrage ?
+// Réponse : 
+// Question: Que se passe-t-il si une variable requise est manquante ?
+// Réponse : 
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const requiredEnvVars = [
+  'MONGODB_URI',
+  'MONGODB_DB_NAME',
+  'REDIS_URI'
+];
+
+// Validation des variables d'environnement
+function validateEnv() {
+  // TODO: Implémenter la validation
+  // Si une variable manque, lever une erreur explicative
+  requiredEnvVars.forEach((varName) => {
+    if (!process.env[varName]) {
+      throw new Error(`La variable d'environnement ${varName} est manquante.
+        Veuillez la définir avant de lancer l'application.`);
+        }
+        });
+}
+validateEnv();
+
+module.exports = {
+  mongodb: {
+    uri: process.env.MONGODB_URI|| 'mongodb://localhost:27017/learning_platform', // Par défaut Mongo local path
+    dbName: process.env.MONGODB_DB_NAME || 'learning_platform',
+  },
+  redis: {
+    uri: process.env.REDIS_URI || 'redis://localhost:6379', // Par défaut Redis local path
+  },
+  port: process.env.PORT || 3000,
+};
