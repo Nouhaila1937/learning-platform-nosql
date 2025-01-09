6b5b647e335b42b94d2317d53b67e293f80ff352
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-01-09 16:56:14 +0100

    app update

--- src/app.js
+++ src/app.js
@@ -10,12 +10,21 @@ const studentRoutes = require('./routes/studentRoutes');
 
 const app = express();
 
+app.use(express.json());
+app.use('/courses', courseRoutes);
 async function startServer() {
   try {
     // TODO: Initialiser les connexions aux bases de données
     // TODO: Configurer les middlewares Express
     // TODO: Monter les routes
     // TODO: Démarrer le serveur
+    await connectMongo();
+    await connectRedis();
+
+    app.listen(config.port, () => {
+      console.log(`Server started on port ${config.port}`);
+      });
+      
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
@@ -25,6 +34,11 @@ async function startServer() {
 // Gestion propre de l'arrêt
 process.on('SIGTERM', async () => {
   // TODO: Implémenter la fermeture propre des connexions
+  // TODO: Arrêter le serveur
+  await db.closeConnections();
+
+  process.exit(0);
+  
 });
 
 startServer();
